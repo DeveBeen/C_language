33b7d1ce5081f49231c9c7523e921d77611ec404
Kimwonbin0921 <pransis1@naver.com>
2021-08-27 20:42:09 +0900

    8M27D

---
+++ level.goorm.io/groom43181.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+	int n = 0;
+	int count = 1;
+	
+	scanf("%d", &n);
+	
+	int refeat = n;
+	
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < refeat; j++)
+		{
+			printf(" ");
+		}
+		for(int k = 0; k < count; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+		refeat = refeat - 1;
+		count = count + 2;
+	}
+}
+
